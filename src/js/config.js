d888a7ec8da92ea9126deb8cc7935c0503d1b70d
Alejandro Udaquiola <alejandroudaquiola1@gmail.com>
2020-11-11 17:47:14 +0100

    Proyect ready to push

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = 'bed2dda8-9621-4008-a91c-bbdb36a244dd';
+export const MODAL_CLOSE_SEC = 2.5;
